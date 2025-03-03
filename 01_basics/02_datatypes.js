fd2cbe6d4f3d8d370a712b7ea9f6db2eaa819817
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-03 18:02:51 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"uss strict";  //treat all js code newer version
+
+    //alert(3+3)    we are node js user not browers
+
+    console.log( 3 
+        + 3 );  //code readaility should be high
+    
+console.log(typeof 54433.7);
+console.log(typeof null);
+console.log(typeof undefined);
+
+/*boolean => true /false
+String=>""
+age=>Number
+Symbol=>unique
+null=>standalone value 
+Number=>2 to power 53
+undefined=>undefined
+null type of object
+undefined type of undefined */
