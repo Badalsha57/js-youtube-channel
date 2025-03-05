bb4bcf7cd465bd0ec0c061e46b3db77ce1080488
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-05 02:14:56 +0000

    conversion data type

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score="badal"
+// let score="badal"
 
 // console.log( typeof score);
 // console.log(typeof(score));
@@ -15,8 +15,38 @@ let score="badal"
 // false => 0
 
 
-let IsLoggedIn = 1
-let booleanIsLoggedIn =Boolean(IsLoggedIn)
-console.log(booleanIsLoggedIn);
+// let IsLoggedIn = 1
+// let booleanIsLoggedIn =Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+///--------operation----------
+
+// let value = 4
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  //2 of power
+// console.log(2/2);
+// console.log(2%2);   //remiader 
+
+// let str1 = "Badal"
+// let str2 = " Sumit"
+
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("2" + "3");   // o/p=23
+// console.log(3 + 4 + "5");  //given o/p = 75
+
+let num1 = 34 , num2 = 53
+let num3 = (num1+num2)
+console.log(num3)
+
+
+
+
 
 
