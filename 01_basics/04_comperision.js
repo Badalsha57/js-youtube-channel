6982237d53d5a9a6d39543da4c08ca68654ac5d5
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-05 02:36:53 +0000

    comparision data type

---
+++ 01_basics/04_comperision.js
@@ -0,0 +1,21 @@
+// console.log(3>2);
+// console.log(3<=2);
+// console.log(3!=2);
+// console.log(3<5);
+
+// Null
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0)
+
+
+// undefined 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0)
+
+/// ===   always chech the data types before excution
+
+
+console.log("2"=== 1);  // false
+console.log(2 === 2);   //true
