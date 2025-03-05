2906884a2f83b385c4bd90814ef0d5ee41480d2b
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-05 07:13:23 +0000

    datatype summary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,40 @@
+// let score = 100;
+// let accountNumber = 367;
+
+// console.log(score);
+// console.log(accountNumber);
+
+// data type
+// premitive datatype
+//  thera are 7 datatype
+//  1.boolean
+//  2.string
+//  3.Number
+//  4.BigInt
+//  5.symbol
+//  6.undefined
+//  7.Null
+
+const Id = Symbol("232");
+const anotherId = Symbol("232");
+console.log(Id === anotherId);
+
+// Reference dtatatype  or  // Non-premitive data-type
+// 1.Fuction
+// 2. object'c
+// 3. Array
+
+const heros = ["saktiman", "father", "mother"];
+let myobj = {
+  Name: "Badal",
+  age: 43,
+};
+const myfunction = function () {
+  console.log("hello world");
+  console.log(age);
+};
+
+// bigNumber = 43476743284u3284n
+
+
+console.log(typeof bigNumber);
