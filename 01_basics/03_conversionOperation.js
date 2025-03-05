5ce8ccec04b7018130b83d8749584b152e724c78
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-05 01:20:21 +0000

    conevresion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="badal"
+
+// console.log( typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// Note
+// "33" => 33
+// 33abc =>  NaN
+// true => 1 
+// false => 0
+
+
+let IsLoggedIn = 1
+let booleanIsLoggedIn =Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
