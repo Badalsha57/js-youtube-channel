e342e0e58e04f5445b126919c427788e44233598
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-07 11:00:13 +0000

    arrays

---
+++ 02_basics/02_Arrays.js
@@ -0,0 +1,17 @@
+const marvel_heros = ["batman","jord","spiderman"];
+const main_heros = ["badal","suraj","price"];
+// console.log(marvel_heros.push())
+// console.log(main_heros.push())
+// marvel_heros.push(main_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+// const laila = marvel_heros.concat(main_heros)  // concat basically use for  the   seperate the one another array element
+// console.log(laila)
+// console.log(laila)
+// const laila4 = [...marvel_heros,...main_heros]
+// console.log(laila4)
+
+const suraj = [2,44,4453,[43,4.43,345,[434,43,4,5,[54,454,46]]]]
+const prince = suraj.flat(Infinity)  //  
+console.log(prince)
