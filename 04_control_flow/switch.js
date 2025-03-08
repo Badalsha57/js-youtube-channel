cee211b3fe22fb7607a9778baf8f0c9dd1c3be69
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-08 16:47:45 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,40 @@
+// syntax f switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// simple program of switch case
+
+// const weekly = "sunday"
+const weekly = 6
+
+switch (weekly) {
+    case 1:
+        console.log("monday")
+        break;
+    case 2:
+        console.log("tuesday")
+        break;
+    case 3:
+        console.log("wednesday")
+        break;
+    case 4:
+        console.log("thursday")
+        break;
+    case 5:
+        console.log("saturday")
+        break;
+    case 6:
+        console.log("sunday")
+        break;
+
+    default:
+        console.log("oop's...! wrong choice")
+        break;
+}
