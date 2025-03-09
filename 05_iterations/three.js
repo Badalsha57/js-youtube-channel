544a91b46786a737fbc32b0496a35d7af20f0804
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-09 14:15:31 +0000

    loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,27 @@
+// for of
+
+// ["","", ""]
+// [{},{},{},]
+
+const arr = [1,23,4,3,3]
+
+for (const num of arr) {
+    // console.log(num)
+}
+
+const greeting = "Hello guys"
+for (const greet of greeting) {
+    // console.log(`good mornning ${greet}`)  
+}
+
+
+// for each loop
+
+const val = ["badal","suraj","prince"]
+// val.forEach(element => {
+    val.forEach(function name (arjun){ // making is a one function compusary
+    console.log(val)  // with squar bracket output
+    console.log(arjun) // without squar bracket output
+    
+});
+
