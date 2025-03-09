ced8f3a97a06e49155a0f6f65a2396f5c55a4ae6
Badal sharma <badalkumarsharma83873@gmail.com>
2025-03-09 06:18:54 +0000

    while or do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+// // while loop
+
+let i = 1
+while (i <= 20) {
+    // console.log("result:" ,i)
+    i = i+2;
+    
+}
+
+// // do while loop
+
+let match = 0
+
+do {
+    console.log(`final match is ${match}`)
+    match++;
+} while (match <= 10);
